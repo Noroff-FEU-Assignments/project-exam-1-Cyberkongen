5b29a81831ed05be3c783589ed4d41572cf27b04
Werner <122488053+Cyberkongen@users.noreply.github.com>
2023-11-05 13:42:55 +0100

    05.11

--- js/carousel.js
+++ js/carousel.js
@@ -1,25 +1,50 @@
-const apiUrl = "https://www.nordicelegance.no/wp-json/wp/v2/posts?_embed"
-const carousel = document.querySelector(".carousel");
-const carouselNav = document.querySelector(".carouselNav");
+document.addEventListener("DOMContentLoaded", function() {
+
+  const apiUrl = "https://www.nordicelegance.no/wp-json/wp/v2/posts?_embed";
+  const carousel = document.querySelector(".carousel");
+  const carouselNav = document.querySelector(".carouselNav");
 
 
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
-    let slideIndex = 1;
+    let slideIndex = 0;
 
     data.forEach((post) => {
-        const carouselImg = document.createElement("img");
-        carouselImg.src = post._embedded["wp:featuredmedia"][0].source_url;
-        carousel.appendChild(carouselImg);
-        carouselImg.id = "slide-" + slideIndex;
+      const carouselImg = document.createElement("img");
+      carouselImg.src = post._embedded["wp:featuredmedia"][0].source_url;
+      carousel.appendChild(carouselImg);
+      carouselImg.id = "slide-" + (slideIndex + 1);
 
-        const carouselButton = document.createElement("a");
-        carouselNav.appendChild(carouselButton);
-        carouselButton.href = "#slide-" + slideIndex;
+      const carouselButton = document.createElement("a");
+      carouselNav.appendChild(carouselButton);
+      carouselButton.href = "#slide-" + (slideIndex + 1);
 
-        slideIndex++;
-    })
+
+      slideIndex++;
+    });
+    const leftArrow = document.getElementById('leftarrow');
+    const rightArrow = document.getElementById('rightarrow');
+    console.log(leftArrow);
+
+    let currentSlideIndex = 0;
+
+    leftArrow.addEventListener('click', () => {
+      // Decrement the current slide index
+      currentSlideIndex = (currentSlideIndex - 1 + data.length) % data.length;
+      window.location.hash = "slide-" + (currentSlideIndex + 1);
+    });
+
+    rightArrow.addEventListener('click', () => {
+      // Increment the current slide index
+      currentSlideIndex = (currentSlideIndex + 1) % data.length;
+      window.location.hash = "slide-" + (currentSlideIndex + 1);
+    });
+
+
   });
+})
 
-
+window.addEventListener('load', function() {
+  window.location.hash = "slide-1";
+});
